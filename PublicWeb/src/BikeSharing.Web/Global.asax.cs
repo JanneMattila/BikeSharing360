41a7c4c887d6f92409a6344cc47fa3f7b817a753
Janne Mattila <janne.mattila@hotmail.fi>
2026-03-21 12:00:42 +0200

    Check backend availability on startup; update .gitignore
    Added a startup check in Global.asax.cs to verify the backend service (from "PrivateWebsite" AppSetting) is reachable before application initialization. Also updated .gitignore to exclude the .nuget/ directory.

--- PublicWeb/src/BikeSharing.Web/Global.asax.cs
+++ PublicWeb/src/BikeSharing.Web/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Web.Mvc;
 using System.Web.Routing;
 using Microsoft.ApplicationInsights.Extensibility;
 
@@ -8,6 +11,21 @@ namespace BikeSharing.Web
     {
         protected void Application_Start()
         {
+            var backendUrl = ConfigurationManager.AppSettings["PrivateWebsite"];
+            if (!string.IsNullOrEmpty(backendUrl))
+            {
+                // Check if the backend is up and running before starting the application
+                var request = WebRequest.CreateHttp(backendUrl);
+                request.Timeout = 5000;
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine("Backend server OK");
+                    }
+                }
+            }
+
             var connectionString = System.Configuration.ConfigurationManager.AppSettings["APPLICATIONINSIGHTS_CONNECTION_STRING"];
             if (!string.IsNullOrEmpty(connectionString))
             {
